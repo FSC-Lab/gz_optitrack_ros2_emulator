6988bf02f67cf674ff586dde7ed73569abeca16d
Longhao Qian <longhao.qian@mail.utoronto.ca>
2025-07-31 17:31:09 -0400

    add license declaration in souce code.

--- include/math_utils.hpp
+++ include/math_utils.hpp
@@ -1,13 +1,27 @@
 /***************************************************************************************************************************
-* math_utils.h
-*
-* Author: Longhao Qian
-*
-* Update Time: 2019.12.10
-*
-* Introduction:  math utils functions 
-*
-*               1、转换 ref to https://github.com/PX4/Matrix/blob/56b069956da141da244926ed7000e89b2ba6c731/matrix/Euler.hpp
+MIT License
+
+Copyright (c) 2025 FSC Lab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+1、转换 ref to https://github.com/PX4/Matrix/blob/56b069956da141da244926ed7000e89b2ba6c731/matrix/Euler.hpp
 ***************************************************************************************************************************/
 #ifndef MATH_UTILS_H
 #define MATH_UTILS_H
